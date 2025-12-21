4169af3ba1538f55c83a2db0ac0ba767362d56bd
Balla Krishnakanth <28777567+krishna00000785@users.noreply.github.com>
2025-12-21 20:11:23 +0530

    added start typing button and onclick operation

--- src/components/TypingArea.tsx
+++ src/components/TypingArea.tsx
@@ -1,10 +1,52 @@
+import { useEffect, useState } from 'react';
+import data from '../assets/data/data.json';
+
 export function TypingArea() {
+
+  const [isStarted, setIsStarted] = useState(false);
+//  const [passage, setPassage] = useState(data.easy[0].text);
+
+//  useEffect(() => {setPassage(data.easy[0].text);}, []);
+
   return (
-    <div className="relative mt-6 rounded bg-neutral-800 p-4">
-        {/* Typing text */}
-        <p className="text-sm text-neutral-400 select-none">
-            Click here and start typing the passage shown here...
-        </p>
+    <div className="relative mt-6 rounded-lg bg-neutral-800 px-4 py-6">
+      {/* content goes here */}
+      <p className={`text-sm leading-relaxed text-neutral-400 ${!isStarted ? 'select-none' : ''}`} >
+        {data.easy[0].text}
+      </p>
+
+      {/* Overlay */}
+      {!isStarted && (
+        <div className='
+          absolute inset-0 
+          flex flex-col 
+          items-center justify-center
+          gap-3 
+          bg-neutral-900/70
+          rounded-lg'
+        >
+
+          <button
+            onClick={() => setIsStarted(true)}
+            className='
+              rounded-md
+              bg-blue-500
+              px-6 py-3
+              text-sm font-semibold text-white
+              hover:bg-blue-600
+              transition
+            '
+          >
+            Start Typing Test
+          </button>
+
+          <p className='text-sm text-neutral-300'>
+            or click the text and start typing
+          </p>
+
+        </div>
+      )}
+
     </div>
   )
 }
--- src/layout/Container.tsx
+++ src/layout/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 export function Container({ children }: ContainerProps) {
   return (
     <div className="min-h-screen bg-neutral-900 text-neutral-0 font-sora">
-      <div className="mx-auto max-w-md px-4 py-6">
+      <div className="mx-auto max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl px-4 py-6">
         {children}
       </div>
     </div>
